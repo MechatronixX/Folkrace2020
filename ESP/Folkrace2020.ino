c657e8dd95a4837b75daf998a062565d488d7ee5
Love Palm (Paxec AB) <par.chalmers@gmail.com>
2020-01-16 20:32:06 +0100

    Initial hello world commit. Can be opened using Visual Studio with the Visual Micro plugin.

---
+++ ESP/Folkrace2020.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		Folkrace2020.ino
+ Created:	1/16/2020 7:31:25 PM
+ Author:	root
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(9600);
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	Serial.println("Hejhejhej");
+  
+}
