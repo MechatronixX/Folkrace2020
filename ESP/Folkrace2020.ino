36af696bc97378b65f3a87c7182dc9fe33ac75c3
Love Palm (Paxec AB) <par.chalmers@gmail.com>
2020-01-16 21:07:24 +0100

    Hello world for VL53 TOF sensor.

--- ESP/Folkrace2020.ino
+++ ESP/Folkrace2020.ino
@@ -1,17 +1,47 @@
 /*
  Name:		Folkrace2020.ino
- Created:	1/16/2020 7:31:25 PM
- Author:	root
+ Created:	1/16/2020 7:31:25 
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-	Serial.begin(9600);
+#include "Adafruit_VL53L0X.h"
 
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+	Serial.begin(115200);
+
+	// wait until serial port opens for native USB devices
+	while (!Serial) 
+	{
+		delay(1);
+	}
+
+	Serial.println("Adafruit VL53L0X test");
+	if (!lox.begin()) 
+	{
+		Serial.println(F("Failed to boot VL53L0X"));
+		while (1);
+	}
+	// power 
+	Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
-	Serial.println("Hejhejhej");
-  
+	VL53L0X_RangingMeasurementData_t measure;
+
+	Serial.print("Reading a measurement... ");
+	lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+	if (measure.RangeStatus != 4) 
+	{  // phase failures have incorrect data
+		Serial.print("Distance (mm): "); 
+		Serial.println(measure.RangeMilliMeter);
+	}
+	else 
+	{
+		Serial.println(" out of range ");
+	}
+
+	delay(100);
 }
